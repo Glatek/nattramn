3466a06c8356c520ca0c8fe794e309879207fc42
Jeremy Karlsson <karlsson@jeremy.se>
2022-06-13 21:55:42 +0200

    fix export

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { serve } from 'https://deno.land/std@0.143.0/http/server.ts';
-import { serveFile } from "https://deno.land/std@0.143.0/http/file_server.ts";
+export { serveFile } from "https://deno.land/std@0.143.0/http/file_server.ts";
 export { extname } from 'https://deno.land/std@0.143.0/path/mod.ts';
 export { readAll } from 'https://deno.land/std@0.143.0/streams/conversion.ts';
