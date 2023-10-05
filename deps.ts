cd13cf696374d82c8842c168b863d14fef1a9e5d
Jeremy Karlsson <karlsson@jeremy.se>
2023-10-05 21:42:21 +0200

    Update std

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { serve } from 'https://deno.land/std@0.143.0/http/server.ts';
-export { serveFile } from "https://deno.land/std@0.143.0/http/file_server.ts";
-export { extname } from 'https://deno.land/std@0.143.0/path/mod.ts';
-export { readAll } from 'https://deno.land/std@0.143.0/streams/conversion.ts';
+export { serve } from 'https://deno.land/std@0.203.0/http/server.ts';
+export { serveFile } from "https://deno.land/std@0.203.0/http/file_server.ts";
+export { extname } from 'https://deno.land/std@0.203.0/path/mod.ts';
+export { readAll } from 'https://deno.land/std@0.203.0/streams/read_all.ts';
